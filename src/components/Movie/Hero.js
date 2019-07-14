bae3d720c6f1bb86768e9a179035342ec61f675e
Tolsee <tolsee3@gmail.com>
2019-07-14 21:15:29 +0545

    Single movie hero section add title, favorite, styling cleanup

--- src/components/Movie/Hero.js
+++ src/components/Movie/Hero.js
@@ -2,13 +2,47 @@ import React from 'react';
 import styled from 'styled-components';
 import Icon from 'antd/lib/icon';
 import { Link } from 'react-router-dom';
+import {H1, Paragraph} from 'components/common/typo';
+import Favorite from 'components/common/Favorite'
 
 const HeroSection = styled.div`
   width: 100%;
   max-height: 80vh;
   overflow: hidden;
   position: relative;
-  z-index: 0;
+  &::after {
+    display: block;
+    position: absolute;
+    background-image: linear-gradient(to bottom, rgba(255, 255, 255, 0) 0, #000 100%);
+    bottom: 0;
+    height: 30vh;
+    width: 100%;
+    content: '';
+  }
+`;
+
+const TitleBar = styled.div`
+  position: absolute;
+  display: flex;
+  justify-content: space-between;
+  bottom: calc(1em + 48px);
+  left: 0;
+  width: 100%;
+  padding: 0 5vw;
+  z-index: 2;
+`;
+
+const Title = styled(H1)`
+  && {
+    color: #fff;
+    margin-bottom: 8px;
+    line-height: 1;
+  }
+`;
+
+const TagLine = styled(Paragraph)`
+  color: #fff;
+  margin-bottom: 0;
 `;
 
 const CoverImg = styled.img`
@@ -24,6 +58,7 @@ const BackLink = styled(Link)`
   width: 48px;
   border-radius: 50%;
   background-color: rgba(0, 0, 0, 0.4);
+  z-index: 999;
 `;
 
 const Back = styled(Icon)`
@@ -38,16 +73,26 @@ const Back = styled(Icon)`
 `;
 
 type CardProps = {
+  id: string;
   src: string;
+  title: string;
+  tagLine: string;
 }
 
-export default function Card({ src }:CardProps) {
+export default function Card({ id, src, title, tagLine }:CardProps) {
   return (
     <HeroSection>
       <BackLink to="/">
         <Back type="arrow-left"/>
       </BackLink>
       <CoverImg src={src} alt="" />
+      <TitleBar>
+        <div>
+          <Title>{title}</Title>
+          <TagLine>{tagLine}</TagLine>
+        </div>
+        <Favorite id={id} />
+      </TitleBar>
     </HeroSection>
   )
 }
