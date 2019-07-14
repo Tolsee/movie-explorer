431ef4c9767f8187c15dc346b83f6001ad13ef08
Tolsee <tolsee3@gmail.com>
2019-07-14 21:14:26 +0545

    Antd typography

--- src/components/common/typo.js
+++ src/components/common/typo.js
@@ -1,15 +1,31 @@
 // @flow
+import React from 'react';
+import type { Node } from 'react';
 import styled from 'styled-components';
+import Typography from 'antd/lib/typography';
 
-export const H1 = styled.h1`
-  font-size: 32px;
-  color: ${({ theme }) => theme.colors.grey};
+const { Title } = Typography;
+
+const StyledTitle = styled(Title)`
+  && {
+    color: ${({theme}) => theme.colors.grey};
+  }
 `;
 
-export const H2 = styled.h2`
-  font-size: 24px;
-  color: ${({ theme }) => theme.colors.grey};
-`;
+type TypoProp = {
+  children: Node;
+  className?: string;
+}
+
+export function H1({ children, className }: TypoProp) {
+  return <StyledTitle className={className}>{children}</StyledTitle>
+}
+
+export function H2({ children, className }: TypoProp) {
+  return <StyledTitle
+    level={2}
+    className={className}>{children}</StyledTitle>
+}
 
 export const Paragraph = styled.p`
   font-size: 20px;
