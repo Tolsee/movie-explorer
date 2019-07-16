4a55a1b6e4674fef42ae10f18712ddbdddb395f8
Tolsee <tolsee3@gmail.com>
2019-07-17 00:03:11 +0545

    Test for loading component

---
+++ src/components/common/__tests__/Loading.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import { render } from 'utils/testUtil';
+import Loading from 'components/common/Loading';
+
+it('renders Loading component', () => {
+  render(<Loading />);
+});
