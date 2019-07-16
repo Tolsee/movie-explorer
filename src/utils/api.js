d6b5096c753c35b6d3287f1dbc744ccb84c317a9
Tolsee <tolsee3@gmail.com>
2019-07-16 20:07:23 +0545

    Add getTrailer api, cleanup

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,7 @@ const API_BASE = process.env.REACT_APP_API_BASE;
 const API_KEY = process.env.REACT_APP_API_KEY;
 
 async function get(url) {
-  const response = await fetch(url);
+  const response = await fetch(`${API_BASE}/${url}`);
   return response.json();
 }
 
@@ -10,18 +10,24 @@ export async function search(searchText, page) {
   let url;
   if (searchText) {
     const searchParams = searchText.split(' ').join('+');
-    url = `${API_BASE}/search/movie?api_key=${API_KEY}&query=${searchParams}&page=${page}`;
+    url = `search/movie?api_key=${API_KEY}&query=${searchParams}&page=${page}`;
   } else {
-    url = `${API_BASE}/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}`;
+    url = `discover/movie?api_key=${API_KEY}&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}`;
   }
 
   return await get(url);
 }
 
 export async function getMovie(id) {
-  return await get(`${API_BASE}/movie/${id}?append_to_response=videos&api_key=${API_KEY}`);
+  return await get(`movie/${id}?append_to_response=videos&api_key=${API_KEY}`);
 }
 
 export async function getSimilarMovies(id) {
-  return await get(`${API_BASE}/movie/${id}/similar?api_key=${API_KEY}`);
+  return await get(`movie/${id}/similar?api_key=${API_KEY}`);
+}
+
+export async function getTrailerVideo(id) {
+  const { results } = await get(`movie/${id}/videos?api_key=${API_KEY}`);
+  const video = results.find(video => video.type === 'Trailer');
+  return video;
 }
