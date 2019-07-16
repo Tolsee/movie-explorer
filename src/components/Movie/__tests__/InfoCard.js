52c265a38120135c3a2b4d3059f9843ec128acc5
Tolsee <tolsee3@gmail.com>
2019-07-17 00:01:45 +0545

    Add tests for Movie related components

--- src/components/Movie/__tests__/InfoCard.js
+++ src/components/Movie/__tests__/InfoCard.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { render } from 'utils/testUtil';
 import InfoCard from 'components/Movie/InfoCard';
 
-const InfoCardProps = {
+const infoCardProps = {
   poster: 'https://via.placeholder.com/500x281',
   overview: 'Test overview',
   genres: [{
@@ -14,18 +14,27 @@ const InfoCardProps = {
     site: 'YouTube'
   },
   rating: 7,
-  voteCount: 100
+  voteCount: 100,
+  status: 'Released',
+  runtime: 100,
+  releaseDate: '2019-06-28'
 };
 
-// TODO Complete Test
-it('renders InfoCard properly', () => {
-  const { container, getByText } = render(<InfoCard {...InfoCardProps}/>);
+it('renders InfoCard component', () => {
+  const formattedReleaseDate = 'June 28, 2019';
+  const formattedTime = '1 hr 40 min';
 
-  expect(container.getElementsByTagName('img')).toHaveLength(1);
-  expect(container.querySelector('img')).toHaveAttribute('src', InfoCardProps.poster);
+  const { container, getByText } = render(<InfoCard {...infoCardProps}/>);
 
-  expect(getByText(InfoCardProps.overview)).toBeInTheDocument();
+  expect(getByText(infoCardProps.overview)).toBeInTheDocument();
+  expect(getByText(infoCardProps.genres[0].name)).toBeInTheDocument();
+  expect(getByText(`${infoCardProps.voteCount} votes`)).toBeInTheDocument();
+  expect(getByText(infoCardProps.status)).toBeInTheDocument();
+  expect(getByText(formattedReleaseDate)).toBeInTheDocument();
+  expect(getByText(formattedTime)).toBeInTheDocument();
 
-  expect(getByText(InfoCardProps.genres[0].name)).toBeInTheDocument();
-
+  expect(container.querySelector('img')).toBeInTheDocument();
+  expect(container.querySelector('img')).toHaveAttribute('src', infoCardProps.poster);
 });
+
+
--- src/components/Movie/__tests__/MovieCard.js
+++ src/components/Movie/__tests__/MovieCard.js
@@ -1,3 +1,34 @@
-it('should pass', () => {
+import React from 'react'
+import { render, fireEvent } from 'utils/testUtil';
+import MovieCard from 'components/Movie/MovieCard';
 
+const movie = {
+  id: '123',
+  coverImg: 'https://via.placeholder.com/500x281',
+  title: 'Test Title',
+  overview: 'Test overview',
+  rating: 12,
+  voteCount: 12300
+};
+
+it('render movie component', () => {
+  const { getByText, container } = render(<MovieCard {...movie}/>);
+
+  expect(getByText(movie.title)).toBeInTheDocument();
+  expect(getByText(movie.overview)).toBeInTheDocument();
+  expect(getByText(`${movie.voteCount} votes`)).toBeInTheDocument();
+  expect(container.querySelector('img')).toBeInTheDocument();
+  expect(container.querySelector('img')).toHaveAttribute('src', movie.coverImg);
+});
+
+it('calls goToMovie when card is clicked', () => {
+  const goToMovie = jest.fn();
+  const { container } = render(<MovieCard {...movie} goToMovie={goToMovie} />);
+
+  fireEvent(container.querySelector('div'), new MouseEvent('click', {
+    bubbles: true,
+    cancelable: true,
+  }));
+
+  expect(goToMovie).toHaveBeenCalledTimes(1);
 });
