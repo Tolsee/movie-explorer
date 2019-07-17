c12a4c46bcd2f2d18f20562d9d859dcbba8406d6
Tolsee <tolsee3@gmail.com>
2019-07-17 23:04:39 +0545

    Add test for Home and List page

---
+++ src/components/page/__tests__/Home.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { render, waitForElementToBeRemoved } from 'utils/testUtil';
+import Home from 'components/page/Home';
+
+const movie = {
+  id: '1234',
+  title: 'Test title',
+  backdrop_path: 'https://via.placeholder.com/500x281',
+  overview: 'Test overview',
+  vote_average: 4,
+  vote_count: 100
+};
+
+beforeEach(() => {
+  jest.spyOn(window, 'fetch');
+});
+
+it('renders no data when no data is found', async () => {
+  const history = jest.fn();
+  const props = { history };
+
+  const mockSuccessResponse = {
+    page: 1,
+    results: [],
+    total_results: 0
+  };
+  const mockJsonPromise = Promise.resolve(mockSuccessResponse); // 2
+  const mockFetchPromise = Promise.resolve({ // 3
+    json: () => mockJsonPromise,
+  });
+
+  window.fetch = jest.fn(() => mockFetchPromise);
+
+  const { container, getByText } = render(<Home {...props} />);
+  await waitForElementToBeRemoved(() =>
+    container.querySelector('.ant-spin')
+  );
+  expect(getByText(new RegExp('We could not find any movie with given search text'))).toBeInTheDocument();
+});
+
+it('renders movie list', async () => {
+  const history = jest.fn();
+  const props = { history };
+
+  const mockSuccessResponse = {
+    page: 1,
+    results: new Array(3).fill(movie),
+    total_results: 0
+  };
+  const mockJsonPromise = Promise.resolve(mockSuccessResponse); // 2
+  const mockFetchPromise = Promise.resolve({ // 3
+    json: () => mockJsonPromise,
+  });
+
+  window.fetch = jest.fn(() => mockFetchPromise);
+
+  const { container, queryAllByTestId } = render(<Home {...props} />);
+  await waitForElementToBeRemoved(() =>
+    container.querySelector('.ant-spin')
+  );
+  expect(queryAllByTestId('movie-card')).toHaveLength(3);
+});
---
+++ src/components/page/__tests__/List.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { render, waitForElementToBeRemoved } from 'utils/testUtil';
+import List from 'components/page/List';
+
+beforeEach(() => {
+  jest.spyOn(window.localStorage.__proto__, 'setItem');
+  jest.spyOn(window.localStorage.__proto__, 'getItem');
+
+  const mockSuccessResponse = {
+    id: '1234',
+    title: 'Test title',
+    backdrop_path: 'https://via.placeholder.com/500x281',
+    overview: 'Test overview',
+    vote_average: 4,
+    vote_count: 100
+  };
+  const mockJsonPromise = Promise.resolve(mockSuccessResponse); // 2
+  const mockFetchPromise = Promise.resolve({ // 3
+    json: () => mockJsonPromise,
+  });
+
+  jest.spyOn(window, 'fetch');
+  window.fetch = jest.fn(() => mockFetchPromise);
+});
+
+it('renders no data when list is empty', async () => {
+  const history = jest.fn();
+  const props = { history, match: { params: { listName: 'favorite' } } };
+
+  window.localStorage.__proto__.setItem = jest.fn();
+  window.localStorage.__proto__.getItem = jest.fn(() => JSON.stringify([]));
+
+  const { container, getByText } = render(<List {...props} />);
+  await waitForElementToBeRemoved(() =>
+    container.querySelector('.ant-spin')
+  );
+  expect(getByText(new RegExp('There is no movies'))).toBeInTheDocument();
+});
+
+it('renders movie list', async () => {
+  const history = jest.fn();
+  const props = { history, match: { params: { listName: 'favorite' } } };
+
+  window.localStorage.__proto__.setItem = jest.fn();
+  window.localStorage.__proto__.getItem = jest.fn(() => JSON.stringify(['123', '12312']));
+
+  const { container, queryAllByTestId } = render(<List {...props} />);
+  await waitForElementToBeRemoved(() =>
+    container.querySelector('.ant-spin')
+  );
+  expect(queryAllByTestId('movie-card')).toHaveLength(2);
+});
